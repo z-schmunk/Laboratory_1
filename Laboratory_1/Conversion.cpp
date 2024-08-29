019726f7c82968cf2544f4254c1462c6e571ec47
z-schmunk <z-schmunk@JLK307-A2>
2024-08-29 09:46:04 -0400

    Add project files.

---
+++ Laboratory_1/Conversion.cpp
@@ -0,0 +1,48 @@
+// conversion.cpp – Zac Schmunk – 29 August 2024
+// program will accept input in the form: number letter (e.g. 32 f)
+// and will convert the specified temperature from Fahrenheit to Celsius
+// or vice versa, depending on input (either c or f)
+#include <iostream>		// import library that allows us to use cin and cout
+using namespace std; // considered "magic" until later...
+const char FAHRENHEIT = 'f';
+const char CELSIUS = 'C';
+int main(void) {
+	// enter "magic formula" for formatting floating-point output values
+	cout.setf( ios::fixed );
+	cout.setf( ios::showpoint );
+	cout.precision( 1 );
+
+	// variable declarations
+	double inputTemperature;
+	double convertedTemperature;
+	char temperatureScale;
+	char convertedScale;
+
+	// get the temperature to be converted
+	cout << "please enter temperature - indicate scale with C or f:";
+	cin >> inputTemperature >> temperatureScale;
+
+	//determine if legal scale was entered
+	if (temperatureScale != FAHRENHEIT && temperatureScale != CELSIUS) {
+		cout << "Sorry - temperature scale must be either C or f.\n";
+		cout << "Press 'Enter' to continue...";
+		cin.ignore();
+		cin.get();
+		return 1;
+	}
+	// determine what conversion formula to use
+	if (temperatureScale == FAHRENHEIT) {
+		convertedTemperature = (5.0 / 9.0) * (inputTemperature - 32.0);
+		convertedScale = CELSIUS;
+	}
+	else { //input was in Celsius 
+		convertedTemperature = 9.0 / 5.0 * inputTemperature + 32.0;
+		convertedScale = FAHRENHEIT;
+	}
+	// present result and terminate program execution
+	cout << convertedTemperature << " " << convertedScale << endl;
+	cout << "Press 'Enter' to continue...";
+	cin.ignore();
+	cin.get();
+	return 0;
+}
